5abaaf84d6370f07cc0d3e70f0463e648e792cf7
Jared <razrsword@gmail.com>
2018-06-14 01:21:44 -0400

    Adjusting lifetime for better firework realism

--- src/main/java/com/example/examplemod/EDClassTransformer.java
+++ src/main/java/com/example/examplemod/EDClassTransformer.java
@@ -11,6 +11,7 @@ import java.util.Iterator;
 import java.util.Map;
 
 import static org.objectweb.asm.Opcodes.SIPUSH;
+import static org.objectweb.asm.Opcodes.BIPUSH;
 
 public class EDClassTransformer implements IClassTransformer {
 
@@ -18,21 +19,97 @@ public class EDClassTransformer implements IClassTransformer {
     public byte[] transform(String arg0, String arg1, byte[] arg2) {
         if (arg0.equals("bkn") || arg0.equals("bkn/a")) {
             System.out.println("********* INSIDE OBFUSCATED Particle TRANSFORMER ABOUT TO PATCH: " + arg0);
-            return patchClassASM(arg0, arg2, true);
+            return patchClassASMCount(arg0, arg2, true);
         }
 
         if (arg0.equals("net.minecraft.client.particle.EffectRenderer")) {
             System.out.println("********* INSIDE Particle TRANSFORMER ABOUT TO PATCH: " + arg0);
-            return patchClassASM(arg0, arg2, false);
+            return patchClassASMCount(arg0, arg2, false);
+        }
+        if (arg0.equals("bkd")) {
+            System.out.println("********* INSIDE OBFUSCATED Particle constructor ABOUT TO PATCH: " + arg0);
+            return patchClassASMLifetime(arg0, arg2, true);
         }
 
+        if (arg0.equals("net.minecraft.client.particle.EntityFireworkSparkFX")) {
+            System.out.println("********* INSIDE Particle constructor ABOUT TO PATCH: " + arg0);
+            return patchClassASMLifetime(arg0, arg2, false);
+        }
         return arg2;
     }
 
-    public byte[] patchClassASM(String name, byte[] bytes, boolean obfuscated) {
+
+    public byte[] patchClassASMLifetime(String name, byte[] bytes, boolean obfuscated) {
 
         String targetMethodName = "";
-        System.out.println("Particles: targetmethodname " + targetMethodName);
+//Our target method
+        targetMethodName = "init";
+
+
+//set up ASM class manipulation stuff. Consult the ASM docs for details
+        ClassNode classNode = new ClassNode();
+        ClassReader classReader = new ClassReader(bytes);
+        classReader.accept(classNode, 0);
+
+//Now we loop over all of the methods declared inside the Explosion class until we get to the targetMethodName "doExplosionB"
+
+        Iterator<MethodNode> methods = classNode.methods.iterator();
+        while (methods.hasNext()) {
+            MethodNode m = methods.next();
+            int fdiv_index = -1;
+            System.out.println("Particles: " + m.name);
+
+//Check if this is doExplosionB and it's method signature is (Z)V which means that it accepts a boolean (Z) and returns a void (V)
+            if (m.name.contains(targetMethodName)) {
+                System.out.println("Particles: " + "********* Inside target constructor (there should only be 1 right?!");
+
+                AbstractInsnNode currentNode = null;
+                AbstractInsnNode targetNode = null;
+
+                @SuppressWarnings("unchecked")
+                Iterator<AbstractInsnNode> iter = m.instructions.iterator();
+
+                int index = -1;
+
+//Loop over the instruction set and find the instruction FDIV which does the division of 1/explosionSize
+                while (iter.hasNext()) {
+                    index++;
+                    currentNode = iter.next();
+
+//Found it! save the index location of instruction FDIV and the node for this instruction
+                    if (currentNode.getOpcode() == BIPUSH) {
+                        targetNode = currentNode;
+                        fdiv_index = index;
+                        // make new instruction list
+                        InsnList toInject = new InsnList();
+//add your own instruction lists: *USE THE ASM JAVADOC AS REFERENCE*
+                        //Change 22000 to whatever you want your particles to be
+                        toInject.add(new IntInsnNode(BIPUSH, 96));
+                        System.out.println(m.instructions.get(fdiv_index-1).getOpcode());
+                        System.out.println(m.instructions.get(fdiv_index).getOpcode());
+                        System.out.println(m.instructions.get(fdiv_index+1).getOpcode());
+                        m.instructions.insertBefore(targetNode, toInject);
+                        m.instructions.remove(targetNode);
+                        System.out.println(m.instructions.get(fdiv_index-1).getOpcode());
+                        System.out.println(m.instructions.get(fdiv_index).getOpcode());
+                        System.out.println(m.instructions.get(fdiv_index+1).getOpcode());
+                    }
+                }
+                System.out.println("Patching Constructor Complete!");
+                break;
+            }
+        }
+
+
+//ASM specific for cleaning up and returning the final bytes for JVM processing.
+        ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
+        classNode.accept(writer);
+        return writer.toByteArray();
+    }
+
+    public byte[] patchClassASMCount(String name, byte[] bytes, boolean obfuscated) {
+
+        String targetMethodName = "";
 //Our target method
         if (obfuscated == true)
             //targetMethodName = "func_78873_a";
@@ -70,9 +147,6 @@ public class EDClassTransformer implements IClassTransformer {
                 while (iter.hasNext()) {
                     index++;
                     currentNode = iter.next();
-                    System.out.println("Particles: currentNode " + currentNode);
-                    System.out.println("Particles: currentNode " + currentNode.getOpcode());
-                    System.out.println("Particles: currentNode " + currentNode.getType());
 
 //Found it! save the index location of instruction FDIV and the node for this instruction
                     if (currentNode.getOpcode() == SIPUSH) {
@@ -83,22 +157,15 @@ public class EDClassTransformer implements IClassTransformer {
 //add your own instruction lists: *USE THE ASM JAVADOC AS REFERENCE*
                         //Change 22000 to whatever you want your particles to be
                         toInject.add(new IntInsnNode(SIPUSH, 22000));
-                        System.out.println("Particles: toinject " + toInject.getFirst().getOpcode());
-                        System.out.println("Particles: toinject " + toInject.getFirst());
-                        System.out.println(m.instructions.get(fdiv_index-1).getOpcode());
-                        System.out.println(m.instructions.get(fdiv_index).getOpcode());
-                        System.out.println(m.instructions.get(fdiv_index+1).getOpcode());
                         m.instructions.insertBefore(targetNode, toInject);
                         m.instructions.remove(targetNode);
-                        System.out.println(m.instructions.get(fdiv_index-1).getOpcode());
-                        System.out.println(m.instructions.get(fdiv_index).getOpcode());
-                        System.out.println(m.instructions.get(fdiv_index+1).getOpcode());
                     }
                 }
-                System.out.println("Patching Complete!");
+                System.out.println("Patching Method Complete!");
                 break;
             }
         }
+
 
 //ASM specific for cleaning up and returning the final bytes for JVM processing.
         ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
